6e8e33a612a25a5e6007daba3a8b96277a898b9f
HmurBolotny <djtestch89@gmail.com>
2022-04-06 14:11:32 +0500

    commit

--- habr_parser.py
+++ habr_parser.py
@@ -2,11 +2,11 @@ import class_web_processor
 import class_text_processor
 import class_collections_processor
 
-HABRURL = 'https://habr.com/ru/post/547472/'
-HABRTAG = {'title', 'span', 'p'}
+#HABRURL = 'https://habr.com/ru/post/547472/'
+#HABRTAG = {'title', 'span', 'p'}
 
 
-def pars():
+def pars(HABRURL, HABRTAG):
     date = class_web_processor.ClientWeb(HABRURL)
     text = class_text_processor.TextHandler(date.get_date(), HABRTAG)
     freq = class_collections_processor.Frequency(text.get_words())
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from habr_parser import pars
 
 
 def main():
-    pars()
+    pars('https://habr.com/ru/post/547472/',{'title', 'span', 'p'})
 
 
 if __name__ == '__main__':
